2136a11f65dca1cb563ce00b65e5209c31cfb673
Robert Cordingly <rcording@uw.edu>
2020-02-26 19:57:00 -0800

    Updates to experiment scripts to have better folder organization

--- test/experiment1.sh
+++ test/experiment1.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# FaaS Programming Languages Comparison Experiment 1
+# @author Robert Cordingly
+
 args="--runs 1 --threads 1 --warmupBuffer 0 --combineSheets 0 --sleepTime 0 --openCSV 0"
+
+subFolder="experiment1_results"
+mkdir ./$subFolder
 
 for file in 100recordExperiment 1000recordExperiment 5000recordExperiment 10000recordExperiment 50000recordExperiment 100000recordExperiment 500000recordExperiment 1000000recordExperiment 1500000recordExperiment
 do
-    mkdir ./$file
+    mkdir ./$subFolder/$file
     for lang in Python Java Go
     do
+        mkdir ./$subFolder/$file/$lang
         for i in 1 2 3 4 5 6 7 8 9 10 11
         do
             echo "----- Running iteration $i of $file in $lang -------"
-            ./faas_runner.py -e ./experiment_s1/$file.json -o $file --function ServiceOne$lang $args
-            ./faas_runner.py -e ./experiment_s2/$file.json -o $file --function ServiceTwo$lang $args
-            ./faas_runner.py -e ./experiment_s3/$file.json -o $file --function ServiceThree$lang $args
+            ./faas_runner.py -e ./experiment_s1/$file.json -o ./$subFolder/$file/$lang --function ServiceOne$lang $args
+            ./faas_runner.py -e ./experiment_s2/$file.json -o ./$subFolder/$file/$lang --function ServiceTwo$lang $args
+            ./faas_runner.py -e ./experiment_s3/$file.json -o ./$subFolder/$file/$lang --function ServiceThree$lang $args
         done
     done
 done
+
+echo "Experiments done"
--- test/experiment3.sh
+++ test/experiment3.sh
@@ -1,63 +1,36 @@
 #!/bin/bash
 
-echo "Running 100 record experiments...."
-
-mkdir ./experiment3_results_100
-output="./experiment3_results_100"
+# FaaS Programming Languages Comparison Experiment 3
+# @author Robert Cordingly
 
 args="--runs 1 --threads 1 --warmupBuffer 0 --combineSheets 0 --sleepTime 0 --openCSV 0"
 
-for i in 1 2 3 4 5 6 7 8 9 10
+subFolder="experiment3_results"
+mkdir ./$subFolder
+
+for file in 100recordExperiment 1500000recordExperiment
 do
-    echo "----- Running iteration $i: -------"
-
-    ./faas_runner.py -e ./experiment_s1/100recordExperiment.json -o $output --function ServiceOnePython $args
-    ./faas_runner.py -e ./experiment_s2/100recordExperiment.json -o $output --function ServiceTwoPython $args
-    ./faas_runner.py -e ./experiment_s3/100recordExperiment.json -o $output --function ServiceThreePython $args
-
-    ./faas_runner.py -e ./experiment_s1/100recordExperiment.json -o $output --function ServiceOneJava $args
-    ./faas_runner.py -e ./experiment_s2/100recordExperiment.json -o $output --function ServiceTwoJava $args
-    ./faas_runner.py -e ./experiment_s3/100recordExperiment.json -o $output --function ServiceThreeJava $args
-
-    ./faas_runner.py -e ./experiment_s1/100recordExperiment.json -o $output --function ServiceOneGo $args
-    ./faas_runner.py -e ./experiment_s2/100recordExperiment.json -o $output --function ServiceTwoGo $args
-    ./faas_runner.py -e ./experiment_s3/100recordExperiment.json -o $output --function ServiceThreeGo $args
-
-    echo "----- Finished iteration $i and sleeping: -------"
-
-    for j in {1..60}
+    mkdir ./$subFolder/$file
+    for lang in Python Java Go
     do
-        echo "Sleeping... $j/60"
-        sleep 60
+        mkdir ./$subFolder/$file/$lang
+        for i in 1 2 3 4 5 6 7 8 9 10 10
+        do
+            echo "----- Running iteration $i of $file in $lang -------"
+            ./faas_runner.py -e ./experiment_s1/$file.json -o ./$subFolder/$file/$lang --function ServiceOne$lang $args
+            ./faas_runner.py -e ./experiment_s2/$file.json -o ./$subFolder/$file/$lang --function ServiceTwo$lang $args
+            ./faas_runner.py -e ./experiment_s3/$file.json -o ./$subFolder/$file/$lang --function ServiceThree$lang $args
+
+        done
+
+        echo "----- Finished iteration $i and sleeping: -------"
+
+        for j in {1..60}
+        do
+            echo "Sleeping... $j/60"
+            sleep 60
+        done
     done
 done
 
-echo "Running 15000000 record experiments...."
-
-mkdir ./experiment3_results_1500000
-output="./experiment3_results_1500000"
-
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-    echo "----- Running iteration $i: -------"
-
-    ./faas_runner.py -e ./experiment_s1/1500000recordExperiment.json -o $output --function ServiceOnePython $args
-    ./faas_runner.py -e ./experiment_s2/1500000recordExperiment.json -o $output --function ServiceTwoPython $args
-    ./faas_runner.py -e ./experiment_s3/1500000recordExperiment.json -o $output --function ServiceThreePython $args
-
-    ./faas_runner.py -e ./experiment_s1/1500000recordExperiment.json -o $output --function ServiceOneJava $args
-    ./faas_runner.py -e ./experiment_s2/1500000recordExperiment.json -o $output --function ServiceTwoJava $args
-    ./faas_runner.py -e ./experiment_s3/1500000recordExperiment.json -o $output --function ServiceThreeJava $args
-
-    ./faas_runner.py -e ./experiment_s1/1500000recordExperiment.json -o $output --function ServiceOneGo $args
-    ./faas_runner.py -e ./experiment_s2/1500000recordExperiment.json -o $output --function ServiceTwoGo $args
-    ./faas_runner.py -e ./experiment_s3/1500000recordExperiment.json -o $output --function ServiceThreeGo $args
-
-    echo "----- Finished iteration $i and sleeping: -------"
-
-    for j in {1..60}
-    do
-        echo "Sleeping... $j/60"
-        sleep 60
-    done
-done
+echo "Experiments done"
