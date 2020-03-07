f392b333c7f6534b87399d7750ed69ce8e7b7017
hanfeiyu <hanfeiyu@uw.edu>
2020-03-07 13:12:53 -0800

    Done experiment 2

--- test/experiment2.sh
+++ test/experiment2.sh
@@ -14,126 +14,68 @@ files="100recordExperiment 1000recordExperiment 5000recordExperiment
 
 args="--runs 1 --threads 1 --warmupBuffer 0 --combineSheets 0 --sleepTime 0 --openCSV 0"
 
-
-#
-# Generate 100 RDS instances for every experiment (100 to 1500000) config JSON files,
-# In total 3*11*100 config files generated.
-#
-
-sub_configs_folder="experiment2_configs"
-
-if [ ! -d "$sub_configs_folder" ]
+sub_results_folder="experiment2_results"
+if [ -d "./$sub_results_folder" ]
 then
-    echo "----------- Create config folder ------------"
-    mkdir ./$sub_configs_folder
-
-    for i in $(seq 1 3)
-    do
-        mkdir ./$sub_configs_folder/experiment_s$i
-        
-        for file in $files
-        do
-            mkdir ./$sub_configs_folder/experiment_s$i/$file
-            
-            for cnt in $(seq 1 100)
-            do
-                echo "----- Generating config files \"sub_configs_folder/experiment_s$i/${file}_admin${cnt}.json\" -------"
-
-                experiment_json=`cat ./experiment_s$i/$file.json | 
-                    awk '/bucketname/ { gsub(/<bucket>/,BUCKET_NAME); print $0; next } { print $0 }' BUCKET_NAME=$BUCKET_NAME | 
-                    awk '/dbName/ { gsub(/""/,DB_NAME); print $0; next } { print $0 }' DB_NAME="\"$DB_NAME\"" |
-                    awk '/dbEndpoint/ { gsub(/""/,DB_ENDPOINT); print $0; next } { print $0 }' DB_ENDPOINT="\"admin$cnt$DB_ENDPOINT\""`
-
-                echo $experiment_json > ./$sub_configs_folder/experiment_s$i/$file/${file}_admin${cnt}.json
-            done
-        done
-    done
+    rm -rf ./$sub_results_folder
+    mkdir ./$sub_results_folder
 else
-    echo "---------- $sub_configs_folder already exists ------------"
+    mkdir ./$sub_results_folder
 fi
 
-#
-# Scale the experiments from 10 to 100, in step 10.
-# Function memory: 3008MB
-# CSV size: 100000
-#
+test_size="100000"
+test_file="100000recordExperiment.json"
+mkdir ./$sub_results_folder/$test_size
 
-sub_results_folder="experiment2_results"
-mkdir ./$sub_results_folder
-
-test_file="100000recordExperiment"
-mkdir ./$sub_results_folder/$test_file
+# Father payloads
+parentPayload1="{\"bucketname\":\"$BUCKET_NAME\",\"key\":\"${test_size}_Sales_Records.csv\"}"
+parentPayload2="{\"bucketname\":\"$BUCKET_NAME\",\"key\":\"edited_${test_size}_Sales_Records.csv\",\"tablename\":\"SalesData\",\"batchSize\":1000,\"dbEndpoint\":\"admin${DB_ENDPOINT}\",\"dbName\":\"$DB_NAME\"}"
+parentPayload3="{\"bucketname\":\"$BUCKET_NAME\",\"key\":\"QueryResults.csv\",\"tablename\":\"SalesData\",\"stressTestLoops\":1,\"dbEndpoint\":\"admin${DB_ENDPOINT}\",\"dbName\":\"$DB_NAME\"}"            
 
 for lang in Python Java Go
 do
-    mkdir ./$sub_results_folder/$test_file/$lang
-    for con in 1 10 20 30 40 50 # Current limitation is 50 instances, should be 100 instances
+    mkdir ./$sub_results_folder/$test_size/$lang
+    for con in 1 5 10 15 20 25 30 35 40 45 50 # Current limitation is 50 instances, should be 100 instances
     do
-        mkdir ./$sub_results_folder/$test_file/$lang/concurrency_$con
+        mkdir ./$sub_results_folder/$test_size/$lang/concurrency_$con
+
+        # Generate payloads
+        payloads1="["
+        payloads2="["
+        payloads3="["
+
+        for con_i in $(seq 1 $con)
+        do
+            payloads1="$payloads1{\"dbEndpoint\":\"admin${con_i}${DB_ENDPOINT}\"}"
+            payloads2="$payloads2{\"dbEndpoint\":\"admin${con_i}${DB_ENDPOINT}\"}"
+            payloads3="$payloads3{\"dbEndpoint\":\"admin${con_i}${DB_ENDPOINT}\"}"
+        
+            if [ "$con_i" -lt "$con" ]
+            then
+                payloads1="$payloads1,"
+                payloads2="$payloads2,"
+                payloads3="$payloads3,"
+            else
+                payloads1="$payloads1]"
+                payloads2="$payloads2]"
+                payloads3="$payloads3]"
+            fi
+        done
+        
+        # Repeat each concurrency number for 11 times
         for i in $(seq 1 11)
         do
             echo "----- Running iteration $i of $con concurrency in $lang -------"
-            mkdir ./$sub_results_folder/$test_file/$lang/concurrency_$con/iteration_$i
-
+            mkdir ./$sub_results_folder/$test_size/$lang/concurrency_$con/iteration_$i
+            
             # ServiceOne
-            mkdir ./$sub_results_folder/$test_file/$lang/concurrency_$con/iteration_$i/ServiceOne 
-            for con_i in $(seq 1 $con)
-            do
-                ./faas_runner.py -e ./$sub_configs_folder/experiment_s1/$test_file/${test_file}_admin$(($con_i)).json -o ./$sub_results_folder/$test_file/$lang/concurrency_$con/iteration_$i/ServiceOne --function ServiceOne$lang $args &    
-            done
+            ./faas_runner.py -o ./$sub_results_folder/$test_size/$lang/concurrency_$con/iteration_$i --function ServiceOne$lang $args --parentPayload $parentPayload1 --payloads $payloads1  
 
-            # Check if ServiceOne is done
-            while 1
-            do
-                ps_ServiceOne=`ps aux | grep ServiceOne$lang | grep -v grep`
-                if [ -z "$ps_ServiceOne" ]
-                then
-                    echo "--------- Iteration $i of $con concurrency in $lang ServiceOne done! --------"
-                    break
-                else
-                    echo "--------- Iteration $i of $con concurrency in $lang ServiceOne still running ----------"
-                fi
-            done
+            # ServiceTwo
+            ./faas_runner.py -o ./$sub_results_folder/$test_size/$lang/concurrency_$con/iteration_$i --function ServiceTwo$lang $args --parentPayload $parentPayload2 --payloads $payloads2   
 
-            # ServiceTwo begins
-            mkdir ./$sub_results_folder/$test_file/$lang/concurrency_$con/iteration_$i/ServiceTwo
-            for con_i in $(seq 1 $con)
-            do 
-                ./faas_runner.py -e ./$sub_configs_folder/experiment_s2/$test_file/${test_file}_admin$(($con_i)).json -o ./$sub_results_folder/$test_file/$lang/concurrency_$con/iteration_$i/ServiceTwo --function ServiceTwo$lang $args &
-            done
-
-            # Check if ServiceTwo is done
-            while 1
-            do
-                ps_ServiceTwo=`ps aux | grep ServiceTwo$lang | grep -v grep`
-                if [ -z "$ps_ServiceTwo" ]
-                then
-                    echo "--------- Iteration $i of $con concurrency in $lang ServiceTwo done! --------"
-                    break
-                else
-                    echo "--------- Iteration $i of $con concurrency in $lang ServiceTwo still running ----------"
-                fi
-            done
-
-            # Service three begins
-            mkdir ./$sub_results_folder/$test_file/$lang/concurrency_$con/iteration_$i/ServiceThree
-            for con_i in $(seq 1 $con)
-            do 
-                ./faas_runner.py -e ./$sub_configs_folder/experiment_s3/$test_file/${test_file}_admin$(($con_i)).json -o ./$sub_results_folder/$test_file/$lang/concurrency_$con/iteration_$i/ServiceThree --function ServiceThree$lang $args &
-            done
-
-            # Check if ServiceThree is done
-            while 1
-            do
-                ps_ServiceThree=`ps aux | grep ServiceThree$lang | grep -v grep`
-                if [ -z "$ps_ServiceThree" ]
-                then
-                    echo "--------- Iteration $i of $con concurrency in $lang ServiceThree done! --------"
-                    break
-                else
-                    echo "--------- Iteration $i of $con concurrency in $lang ServiceThree still running ----------"
-                fi
-            done
+            # ServiceThree
+            ./faas_runner.py -o ./$sub_results_folder/$test_size/$lang/concurrency_$con/iteration_$i --function ServiceThree$lang $args --parentPayload $parentPayload3 --payloads $payloads3
         done
     done
 done
