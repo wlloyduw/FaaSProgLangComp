f2681888153f714d84400559ffd3964914be2f47
Zohreh Sadeghi <zsadeghi@uw.edu>
2020-04-26 22:29:50 -0700

    Optimized package script

--- nodejs/package.sh
+++ nodejs/package.sh
@@ -8,8 +8,15 @@ wd=$(mktemp -d)
 
 echo "Copying target files ..."
 cp -r ./src "${wd}"
-grep -v "aws-sdk" "./package.json" > "${wd}/package.json"
-
+grep -v "aws-sdk" "./package.json" > "${wd}/package.json.tmp"
+devDependenciesStart=$(grep -n devDependencies "${wd}/package.json.tmp" | cut -d':' -f 1)
+devDependenciesCount=$(tail -n +"${devDependenciesStart}" "${wd}/package.json.tmp" | grep -n '}' | head -n 1 | cut -d':' -f 1)
+devDependenciesCount=$(( devDependenciesCount + devDependenciesStart ))
+devDependenciesStart=$(( devDependenciesStart - 1 ))
+head -n +"${devDependenciesStart}" "${wd}/package.json.tmp" > "${wd}/package.json"
+tail -n +"${devDependenciesCount}" "${wd}/package.json.tmp" >> "${wd}/package.json"
+rm "${wd}/package.json.tmp"
+cat "${wd}/package.json"
 echo "Preparing target package ..."
 pp=$(pwd)
 cd "${wd}" || exit 1
