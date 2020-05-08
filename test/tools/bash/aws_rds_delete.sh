d1d9ce0148668290aae612fd257fc0231f582400
varikmp <varikmp@uw.edu>
2020-05-07 23:00:34 -0700

    added script to delete multiple RDS instances

---
+++ test/tools/bash/aws_rds_delete.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# AWS Lambda client deleting RDS instances
+# requires awscli
+#
+# script requires packages:
+# apt install awscli
+
+INSTANCES_NO=50
+MIN_CAP=1
+MAX_CAP=32
+AUTO_PAUSE=300
+
+VPC_SEC_GROUP_ID=sg-XXXXXXXX
+MASTER_USERNAME=admin
+MASTER_PASSWORD=password
+
+while [ $INSTANCES_NO -gt 0 ]
+do
+	RESULT=$(aws rds delete-db-cluster --db-cluster-identifier admin$COUNT --skip-final-snapshot 2>&1)
+	INSTANCES_NO=$(($INSTANCES_NO - 1))
+	sleep 5 # avoid network latency from AWS (similar to GCloud)
+done
+exit
+
+
